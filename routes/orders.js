64114a7d929569ecfa7539f782abfd5edbc274e0
Wazius <savickasminijus1@gmail.com>
2019-05-18 12:20:35 +0300

    admin

--- routes/orders.js
+++ routes/orders.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.redirect('orders');
 });
 
-router.get('/orders', function(req, res, next){
+router.get('/orders', isLoggedIn, function(req, res, next){
     Order.find({})
     .exec(function(err, list_orders){
       if (err) {return next(err)};
@@ -25,7 +25,7 @@ router.get('/order/:id', isLoggedIn, function(req, res){
     });  
   });
 
-router.get('/orders/create', isLoggedIn, function(req, res){
+router.get('/orders/create', isLoggedIn, Admin, function(req, res){
    res.render('../views/pages/create');
  });
 
@@ -49,7 +49,7 @@ router.post('/orders/create', isLoggedIn, function(req, res){
   res.redirect('/');
 });
 
-router.post('/order/:id/done', isLoggedIn, function(req, res){
+router.post('/order/:id/done', isLoggedIn, Admin, function(req, res){
   Order.findOneAndUpdate({_id: (req.params.id)}, { $set: { isDone: true } }, { returnOriginal: false }, (err, result) => {
       if (err)
         console.log(err);
@@ -59,7 +59,7 @@ router.post('/order/:id/done', isLoggedIn, function(req, res){
   });
 });
 
-router.post('/order/:id/revert', isLoggedIn, function(req, res){
+router.post('/order/:id/revert', isLoggedIn, Admin, function(req, res){
   Order.findOneAndUpdate({_id: (req.params.id)}, { $set: { isDone: false } }, { returnOriginal: false }, (err, result) => {
       if (err)
         console.log(err);
@@ -69,7 +69,7 @@ router.post('/order/:id/revert', isLoggedIn, function(req, res){
   });
 });
 
-router.get('/order/:id/edit', isLoggedIn, function(req, res){
+router.get('/order/:id/edit', isLoggedIn, Admin, function(req, res){
   Order.findById(req.params.id)
   .exec(function(err, detail){
     if(err) {return next(err)};
@@ -77,7 +77,7 @@ router.get('/order/:id/edit', isLoggedIn, function(req, res){
   });
 });
 
-router.post('/order/:id/edit',isLoggedIn, function(req, res){
+router.post('/order/:id/edit',isLoggedIn, Admin, function(req, res){
   var itemData = {
     name: req.body.name,
     sauce: req.body.sauce,
@@ -118,7 +118,7 @@ function Admin(req, res, next){
   if(req.user.isAdmin === true){
     return next();
   }
-  req.flash("delete", "Neturite leidimo istrinti!");
+  req.flash("delete", "Neturite leidimo!");
   res.redirect('/orders');
 };
 module.exports = router;
